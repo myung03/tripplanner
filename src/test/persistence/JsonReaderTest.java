1229e939dc7b4e82fefbbb90a91732498de72f07
Matthew <matthewyungisworking@gmail.com>
2023-03-04 17:19:32 -0800

    created test data

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,4 +1,37 @@
 package persistence;
 
+import model.Trip;
+import model.Trips;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class JsonReaderTest {
+
+    @Test
+    void testReaderNonExistentFileTrips() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            Trips trips = reader.readTrips();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+
+    @Test
+    void testReaderNonExistentFileTrip() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            Trip trip = reader.readTrip();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
 }
